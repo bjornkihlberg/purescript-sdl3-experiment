5807afa7e9abf2de788590bac0532f42b43412e3
Björn Kihlberg <bjorn.wilhelm.kihlberg@gmail.com>
2026-01-28 13:10:22 +0100

    whatevs

--- src/Main.purs
+++ src/Main.purs
@@ -9,6 +9,7 @@ import Effect (IO, Effect)
 import Effect as Effect
 import Foreign.Ptr as Foreign
 import SDL3.Effect as SDL3.Effect
+import SDL3.Error as SDL3.Error
 import SDL3.Init as SDL3.Init
 import SDL3.Video as SDL3.Video
 
@@ -20,10 +21,13 @@ foreign import println :: forall (e :: Row Effect).
 main :: IO (console :: ConsoleEffect, sdl3 :: SDL3.Effect.SDL3) Unit
 main = Effect.do
   isSdlInit :: Boolean <- SDL3.Init.init
-  mSDL3Window :: Maybe (Foreign.Ptr SDL3.Video.Window) <- SDL3.Video.createWindow "Hello, window" 800 600
-  case mSDL3Window of
-    Maybe.Just sdl3Window -> do
-      SDL3.Video.destroyWindow (Trace.dbg sdl3Window)
-    Maybe.Nothing -> Effect.pure unit
-  Effect.pure $ Trace.dbg isSdlInit
-  SDL3.Init.quit
+  if isSdlInit
+  then Effect.do
+    mSDL3Window :: Maybe (Foreign.Ptr SDL3.Video.Window) <- SDL3.Video.createWindow "Hello, window" 800 600
+    case mSDL3Window of
+      Maybe.Just sdl3Window ->
+        SDL3.Video.destroyWindow (Trace.dbg sdl3Window)
+      Maybe.Nothing -> Effect.pure unit
+    Effect.pure $ Trace.dbg isSdlInit
+    SDL3.Init.quit
+  else Effect.pure unit
